ae922c8fabb13c5940687445c77199dec259d944
ows4444 <ows4444@gmail.com>
2019-05-02 14:59:17 +0500

    Update Channel.js

--- Youtube/Channel.js
+++ Youtube/Channel.js
@@ -1,12 +1,7 @@
 /* eslint-disable prefer-promise-reject-errors */
 const Youtube = require('youtube-api');
 class Channel {
-  GetById(id) {
-    return new Promise((resolve, reject) => {
-      Youtube.channels.list({ part: 'snippet', id }, (_err, _data) =>
-        !_err ? resolve(_data) : reject(_err)
-      );
-    });
-  }
+  GetById(id) { return new Promise((resolve, reject) => id && typeof (id) === 'string' ? Youtube.channels.list({ part: 'snippet', id }, (_err, _data) => !_err ? _data.items.length ? resolve(_data.items[0]) : reject('No Channel Found') : reject(_err)) : reject('Give id In String')) }
+  GetByUserName(forUsername) { return new Promise((resolve, reject) => id && typeof (id) === 'string' ? Youtube.channels.list({ part: 'snippet', forUsername }, (_err, _data) => !_err ? _data.items.length ? resolve(_data.items[0]) : reject('No Channel Found') : reject(_err)) : reject('Give id In String')) }
 }
 module.exports = new Channel();
